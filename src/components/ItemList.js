31164b468edf25c81ca6a1f720e42be9fad9a8d1
Pe-ngo <jamespengo95@gmail.com>
2022-09-14 14:18:44 +0300

    Add useState and useEffect

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,6 +1,16 @@
+import { useState, useEffect } from "react";
+
 
 
 
 function ItemList() {
-    
+    const [items, setItems] = useState([]) 
+
+
+    useEffect(() => {
+        fetch("http://localhost:9292/items")
+        .then((res) => res.json())
+        .then((items) => setItems(items));
+    }, []);
+    console.log(items)
 }
